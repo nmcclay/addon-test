d3952c99718e84be806841c4a5b1296cc9779552
nmcclay <nmcclay@proofpoint.com>
2019-06-13 16:10:35 -0400

    updated with sub-component

---
+++ addon/components/zee-bee.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import layout from '../templates/components/zee-bee';
+
+export default Component.extend({
+  layout: layout,
+  tagName: 'small',
+  classNames: ['zee-bee']
+});
